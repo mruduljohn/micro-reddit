44031c21e42d5b50e4a08366df2395bef09e74d9
Mrudul John <mrudulmathews@gmail.com>
2024-05-03 19:27:34 +0530

    Added Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
