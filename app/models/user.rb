778077513a2bf8a1dcaa5b0ceb5b7e5836434525
Mrudul John <mrudulmathews@gmail.com>
2024-05-03 19:37:24 +0530

    renamed name to  username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :comments
-  validates :name, presence: true
+  validates :username, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_digest, presence: true, length: { minimum: 6 }
 end
