9562315eed8114ab072fb90f4283cb77d4ba2ca6
Mrudul John <mrudulmathews@gmail.com>
2024-05-03 19:59:22 +0530

    working through console

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates :body, presence: true, length: { minimum: 5 }
+  validates :content, presence: true, length: { minimum: 5 }
 end
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   validates :title, presence: true, length: { minimum: 5 }
-  validates :body, presence: true, length: { minimum: 10 }
+  validates :content, presence: true, length: { minimum: 10 }
 end
